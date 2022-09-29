52faf1831cdc9ac8c6c21f19cbbf5697ed6d3fa9
João Teves <68256316+Teves-web@users.noreply.github.com>
2022-09-29 17:50:58 -0300

    Add files via upload

---
+++ ScriptHTML_Foto/processsa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+require "./Bibliotecas/PHPMailer/Exception.php";
+require "./Bibliotecas/PHPMailer/OAuth.php";
+require "./Bibliotecas/PHPMailer/PHPMailer.php";
+require "./Bibliotecas/PHPMailer/POP3.php";
+require "./Bibliotecas/PHPMailer/SMTP.php";
+
+require "./Bibliotecas/processa_envio.php";
